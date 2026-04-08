53261fa30d959ffa3c403eff43cbcd7265034f1c
mamiandr <mamiandr@.42antananarivo.mg>
2026-04-08 18:17:32 +0300

    ex0

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,28 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    ft_command_quest.py                                :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: mamiandr <mamiandr@student.42antananari    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/04/08 18:05:18 by mamiandr          #+#    #+#              #
+#    Updated: 2026/04/08 18:15:45 by mamiandr         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+import sys
+
+def ft_command_quest(arg:list ):
+    print("=== Command Quest ===")
+    print(f"Program name: {arg[0]}")
+    if (len(arg) <= 1):
+        print("No arguments provided!")
+    else:
+        print(f"Arguments received: {len(arg) - 1}")
+        for i in range(1,len(arg)):
+            print(f"Argument {i}: {arg[i]}")
+    print(f"Total arguments: {len(arg)}")
+
+if __name__ == "__main__":
+    ft_command_quest(sys.argv)
+
